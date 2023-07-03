0ad3deab1e100c4c387f0f71194bcdeba821a51a
abeltsew <35835135+abeltsew@users.noreply.github.com>
2023-07-03 15:34:20 +0300

    Group Tasks and Menu Setup
    Co-Authored-By: Rushma <2181562+rushma@users.noreply.github.com>
    Co-Authored-By: Abdurahim .M <Abdugreat3@gmail.com>

---
+++ app.rb
@@ -0,0 +1,42 @@
+require_relative 'lib/manage_books'
+require_relative 'lib/manage_music'
+require_relative 'lib/manage_games'
+
+class App
+  attr_accessor :books, :music, :games
+
+  def initialize
+    @books = []
+    @music = []
+    @games = []
+  end
+
+  def load_data
+    puts 'Loading data here..'
+  end
+
+  def save_exit
+    puts 'Goodbye'
+
+    # save_books
+
+    # save_music
+
+    # save_games
+
+    exit
+  end
+
+  def invalid_option
+    puts 'Invalid option'
+  end
+
+  def options
+    puts 'Please enter the number for the task you want to perform'
+    puts 'choose one of the following'
+    puts '1 - Manage books.'
+    puts '2 - Manage Music.'
+    puts '3 - Manage Games.'
+    puts '4 - Exit'
+  end
+end
